dda0a2d2b22546461d5b90d81f4aeb0e5ba5e87f
homework <ian061504@gmail.com>
2016-10-03 14:43:09 +0800

    java homework

---
+++ src/home_work/replace.java
@@ -0,0 +1,14 @@
+package home_work;
+
+public class replace {
+
+	public static void main(String[] args) {
+		String word1 = "let it go! let it go! cannot hold it back anymore";
+		word1 = word1.replace("it", "\"her\"");
+		word1 = word1.replace("cannot", "Can't");
+		System.out.println(word1);
+	
+
+	}
+
+}
